79578b00585fc5ef43df5ba1b7e92a1fd307f3e8
luqmannnn <luqmannurhakimt@gmail.com>
2019-07-27 19:51:42 +0800

    Create IAccountDao
    Interface class

---
+++ src/main/java/com/example/dao/IAccountDao.java
@@ -0,0 +1,16 @@
+package com.example.dao;
+
+import com.example.pojo.Account;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created by luqman on 27/7/2019.
+ */
+public interface IAccountDao {
+    List<Account> getAllAccounts();
+    int getCountRows();
+    Account getAccount(String uname) throws SQLException;
+    boolean insertStatement(Object object);
+}
