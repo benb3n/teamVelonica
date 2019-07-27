787dedd468e3e79f8a5237e76acd21e06b5efc00
Mich <michelle.tee93@gmail.com>
2019-07-27 20:08:32 +0800

    Implemented access insert and retrieve REST API.

--- src/main/java/com/example/controllers/IRegistrationService.java
+++ src/main/java/com/example/controllers/IRegistrationService.java
@@ -1,9 +1,18 @@
 package com.example.controllers;
 
+import com.example.pojo.Access;
 import com.example.pojo.Account;
+
+import java.util.List;
 
 public interface IRegistrationService {
     boolean create(Account resource);
 
+    boolean createAccess(Access access);
+
     Account retrieve(String username);
+
+    Account retrieve(int id);
+
+    List<Access> retrieveAllAccesses(int id);
 }
